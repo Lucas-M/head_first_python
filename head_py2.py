8fdae05e4808d7ac045a3bcca6a8490741f80778
Luke <lucas.macd@gmail.com>
2016-02-17 13:23:03 -0500

    initial commit

---
+++ head_py2.py
@@ -0,0 +1,17 @@
+# Chapter 2
+# Page 36
+
+def print_lol(the_list):
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+
+
+movies = ["The Holy Grail", 1975, "Terry Jones", 91,
+             ["Graham Chapman", ["Michail Palin", "John Cleese",
+                   "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+print (movies)
+print_lol(movies)
